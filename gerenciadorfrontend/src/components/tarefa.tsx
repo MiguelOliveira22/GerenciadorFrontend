051d5a50c37f5f998226d217ee9b7cbc5538adf6
MiguelQueiroz <miguelqueiroz360@gmail.com>
2025-07-04 23:32:31 -0300

    Carrega sozinho a pagina da tarefa

--- gerenciadorfrontend/src/components/tarefa.tsx
+++ gerenciadorfrontend/src/components/tarefa.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { deletarTarefa } from "@/actions/formularioActions";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Tarefa({
@@ -12,23 +13,15 @@ export default function Tarefa({
 }>) {
     const [ id ] = useState(idIn);
 
-    async function onClick() {
-        if (await deletarTarefa(id)) {
-            // reload
-        }
-        else {
-            // toast falando que deu ruim
-        }
-    }
-
     return (
-        <div className="card">
-            <h2>{titulo}</h2>
-            <p>{descricao}</p>
-            <br/>
-            <p>{status}</p>
-            <small>Criado em: {dataCriacao}</small>
-            <button onClick={onClick}>Apagar</button>
-        </div>
+        <Link href={`/tarefa/${id}`}>
+            <div className="card">
+                <h2>{titulo}</h2>
+                <p>{descricao}</p>
+                <br/>
+                <p>{status}</p>
+                <small>Criado em: {dataCriacao}</small>
+            </div>
+        </Link>
     );
 }
