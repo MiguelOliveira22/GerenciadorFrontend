cd1a2f366ca0b85e89de34320ed7de622c4dbdcc
MiguelQueiroz <miguelqueiroz360@gmail.com>
2025-07-04 23:00:38 -0300

    layout que esqueci de enviar junto

---
+++ gerenciadorfrontend/src/components/logoText.tsx
@@ -0,0 +1,7 @@
+export default function LogoText({ className } : Readonly<{ className?: string }>) {
+    return (
+        <div className={className}>
+            <h1>O Gerenciador</h1>
+        </div>
+    );
+}
